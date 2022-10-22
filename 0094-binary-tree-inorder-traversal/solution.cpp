aadd3aa1312e25c4356a783568195993b9b650dc
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2022-10-22 20:49:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.4 MB (100.00%)

---
+++ 0094-binary-tree-inorder-traversal/solution.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        vector<TreeNode*> Stack;
+        TreeNode *node=root;
+        while(true){
+            if(node){
+                Stack.push_back(node);
+                node=node->left;
+            }
+            else{
+                if(Stack.empty())
+                    break;
+                node=Stack.back();
+                Stack.pop_back();
+                result.push_back(node->val);
+                node=node->right;
+            }
+        }
+        return result;
+    }
+};
