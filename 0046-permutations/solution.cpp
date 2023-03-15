d9488dfaad4091be0596813bce9e1e4e0df60138
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-03-15 19:30:34 +0000

    [LeetCode Sync] Runtime - 3 ms (28.37%), Memory - 7.7 MB (100.00%)

--- 0046-permutations/solution.cpp
+++ 0046-permutations/solution.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int map[nums.size()];
-        for(int i=0;i<nums.size();i++){
-            map[i]=0;
-        }
-        vector<int> ds;
         vector<vector<int>> res;
-        permutation(nums,res,map,ds);
+        perm(nums,0,res);
         return res;
     }
-    void permutation(vector<int> nums,vector<vector<int>>& res,int *map,vector<int>& ds){
-        if(ds.size()==nums.size()){
-            res.push_back(ds);
+    void perm(vector<int>& nums,int ind,vector<vector<int>>& res){
+        if(ind==nums.size()){
+            res.push_back(nums);
             return;
         }
-        for(int i=0;i<nums.size();i++){
-            if(map[i]!=0)continue;
-            ds.push_back(nums[i]);
-            map[i]=1;
-            permutation(nums,res,map,ds);
-            ds.pop_back();
-            map[i]=0;
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            perm(nums,ind+1,res);
+            swap(nums[ind],nums[i]);
         }
     }
+    
 };
