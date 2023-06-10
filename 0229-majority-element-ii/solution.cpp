080f7474bf5e931276a2c4f207152cb6f38fd9c4
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-06-10 20:27:14 +0000

    [LeetCode Sync] Runtime - 11 ms (21.00%), Memory - 15.9 MB (100.00%)

---
+++ 0229-majority-element-ii/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1=0,cnt2=0;
+        vector<int> ans;
+        int el1,el2;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(cnt1==0 && nums[i]!=el2){
+                cnt1=1;
+                el1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=el1){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(nums[i]==el1){
+                cnt1++;
+            }
+            else if(nums[i]==el2){
+                cnt2++;
+            }
+            else{
+                cnt1--;cnt2--;
+            }
+        }
+        cnt2=cnt1=0;
+        for(int i=0;i<n;i++){
+            if(el1==nums[i])
+            cnt1++;
+            else if(el2==nums[i])
+            cnt2++;
+        }
+        if(cnt1>n/3)
+        ans.push_back(el1);
+        if(cnt2>n/3)
+        ans.push_back(el2);
+        return ans;
+    }
+};
