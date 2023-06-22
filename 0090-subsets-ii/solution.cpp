78885e111ae10e04871cb15483728c1ca3813068
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-06-22 20:11:04 +0000

    [LeetCode Sync] Runtime - 7 ms (7.21%), Memory - 7.8 MB (100.00%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -4,15 +4,15 @@ public:
         vector<int> ds;
         vector<vector<int>> res;
         sort(nums.begin(),nums.end());
-        subset(nums,0,ds,res);
+        subset(nums,0,res,ds);
         return res;
     }
-    void subset(vector<int> nums,int ind,vector<int>& ds,vector<vector<int>>& res){
+    void subset(vector<int> nums,int ind,vector<vector<int>> &res,vector<int> &ds){
         res.push_back(ds);
         for(int i=ind;i<nums.size();i++){
             if(i!=ind && nums[i]==nums[i-1])continue;
             ds.push_back(nums[i]);
-            subset(nums,i+1,ds,res);
+            subset(nums,i+1,res,ds);
             ds.pop_back();
         }
     }
