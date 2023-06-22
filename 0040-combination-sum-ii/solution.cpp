e49c9730a2eb235c0783d34c51588a33887cdd61
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-06-22 21:09:05 +0000

    [LeetCode Sync] Runtime - 7 ms (11.26%), Memory - 13.4 MB (99.99%)

--- 0040-combination-sum-ii/solution.cpp
+++ 0040-combination-sum-ii/solution.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<int> ds;
         vector<vector<int>> res;
-        combSum2(candidates,0,target,ds,res);
+        vector<int> ds;
+        sort(candidates.begin(),candidates.end());
+        cs(candidates,target,0,ds,res);
         return res;
     }
-    void combSum2(vector<int>& arr,int ind,int target,vector<int> &ds,vector<vector<int>> &res){
-            if(target==0){
-                res.push_back(ds);
-                return;
-            }
-        for(int i=ind;i<arr.size();i++){
-            if(i>ind && arr[i]==arr[i-1]){
-               continue; 
-            } 
-            if(arr[i]>target){
-               break; 
-            } 
-            ds.push_back(arr[i]);
-            combSum2(arr,i+1,target-arr[i],ds,res);
+    void cs(vector<int> candidates,int target,int ind,vector<int> &ds,vector<vector<int>> &res){
+        if(target==0){
+            res.push_back(ds);
+            return;
+        }
+        for(int i=ind;i<candidates.size();i++){
+            if(i!=ind && candidates[i]==candidates[i-1])continue;
+            if(candidates[i]>target)break;
+            ds.push_back(candidates[i]);
+            cs(candidates,target-candidates[i],i+1,ds,res);
             ds.pop_back();
         }
     }
