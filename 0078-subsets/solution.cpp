b9118197acab34dfc8d9f2422bca413eda849998
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-06-22 20:15:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 15.3 MB (19.56%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> out;
-        subs(nums,out,0,res);
+        vector<int> ds;
+        subset(nums,0,ds,res);
         return res;
     }
-    void subs(vector<int> inp,vector<int> out,int i,vector<vector<int>>& res){
-        if(i>=inp.size()){
-            res.push_back(out);
+    void subset(vector<int> nums,int ind,vector<int> &ds, vector<vector<int>> &res){
+        if(ind==nums.size()){
+            res.push_back(ds);
             return;
         }
-        out.push_back(inp[i]);
-        subs(inp,out,i+1,res);
-        out.pop_back();
-        subs(inp,out,i+1,res);
+        ds.push_back(nums[ind]);
+        subset(nums,ind+1,ds,res);
+        ds.pop_back();
+        subset(nums,ind+1,ds,res);
     }
 };
