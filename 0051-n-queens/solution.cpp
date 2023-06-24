265dbfcb74ba746d714372e62a46227f17061f67
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-06-24 23:03:24 +0000

    [LeetCode Sync] Runtime - 8 ms (15.53%), Memory - 7.2 MB (100.00%)

---
+++ 0051-n-queens/solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++)
+        board[i]=s;
+        solve(0,board,ans,n);
+        return ans;
+    }
+    void solve(int col,vector<string> &board,vector<vector<string>> &ans,int n){
+        if(col==n){
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(isSafe(board,row,col,n)){
+                board[row][col]='Q';
+                solve(col+1,board,ans,n);
+                board[row][col]='.';
+            }
+        }
+    }
+    bool isSafe(vector<string> &board,int row,int col,int n){
+        int i=row,j=col;
+        while(j>=0){
+            if(board[i][j]=='Q')
+            return false;
+            j--;
+        }
+        i=row;j=col;
+        while(i>=0 && j>=0){
+            if(board[i][j]=='Q')
+            return false;
+            i--;j--;
+        }
+        i=row;j=col;
+        while(i<n && j>=0){
+            if(board[i][j]=='Q')
+            return false;
+            i++;j--;
+        }
+        return true;
+    }
+};
