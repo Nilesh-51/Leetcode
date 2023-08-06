9083a7589eb7606872090dd31b55957914736232
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2023-08-06 00:25:14 +0000

    [LeetCode Sync] Runtime - 8 ms (7.47%), Memory - 21.4 MB (99.94%)

---
+++ 0108-convert-sorted-array-to-binary-search-tree/solution.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        TreeNode *root=create(nums,0,nums.size()-1);
+        return root;
+    }
+    TreeNode* create(vector<int> &nums,int l,int r){
+        if(l>r)
+        return NULL;
+        int mid=(l+r)/2;
+        TreeNode *temp=new TreeNode(nums[mid]);
+        temp->left=create(nums,l,mid-1);
+        temp->right=create(nums,mid+1,r);
+        return temp;
+    }
+};
