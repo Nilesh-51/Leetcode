f990d9ef51dfac71eb26ea51860665aaf7deac3e
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2024-11-17 20:12:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.4 MB (100.00%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int func(vector<int>& nums,int i,vector<int> &dp){
-        if(i>nums.size()-1)
-        return 0;
-        if(i==nums.size()-1)
-        return nums[i];
-        if(dp[i]!=-1)
-        return dp[i];
-        int pick=func(nums,i+1,dp);
-        int notPick=nums[i]+func(nums,i+2,dp);
-        return dp[i]=max(pick,notPick);
-    }
+    // int func(vector<int>& nums,int i,vector<int> &dp){
+    //     if(i>nums.size()-1)
+    //     return 0;
+    //     if(i==nums.size()-1)
+    //     return nums[i];
+    //     if(dp[i]!=-1)
+    //     return dp[i];
+    //     int pick=func(nums,i+1,dp);
+    //     int notPick=nums[i]+func(nums,i+2,dp);
+    //     return dp[i]=max(pick,notPick);
+    // }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return func(nums,0,dp);
+        int first=0;
+        int second=nums[0];
+        for(int i=1;i<nums.size();i++){
+            int pick=nums[i];
+            if(i-2>=0){
+                pick+=first;
+            }
+            int notPick=second;
+            first=second;
+            second=max(pick,notPick);
+        }
+        return second;
     }
 };
