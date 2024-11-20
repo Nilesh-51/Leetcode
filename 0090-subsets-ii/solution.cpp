afe8e2e603749af782392baf03bca85cadfc7e33
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2024-11-20 06:29:45 +0000

    [LeetCode Sync] Runtime - 7 ms (7.21%), Memory - 16 MB (10.87%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> ds;
-        vector<vector<int>> res;
-        sort(nums.begin(),nums.end());
-        subset(nums,0,res,ds);
-        return res;
-    }
-    void subset(vector<int> nums,int ind,vector<vector<int>> &res,vector<int> &ds){
-        res.push_back(ds);
-        for(int i=ind;i<nums.size();i++){
-            if(i!=ind && nums[i]==nums[i-1])continue;
-            ds.push_back(nums[i]);
-            subset(nums,i+1,res,ds);
-            ds.pop_back();
+    void func(vector<int> nums,vector<vector<int>>& res,vector<int> temp,int i){
+        if(nums.size()==i){
+            res.push_back(temp);
+            return;
         }
+        temp.push_back(nums[i]);
+        func(nums,res,temp,i+1);
+        temp.pop_back();
+        while(i<nums.size()-1 && nums[i]==nums[i+1])
+        i++;
+        func(nums,res,temp,i+1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> temp;
+        sort(nums.begin(),nums.end());
+        func(nums,res,temp,0);
+        return res;
     }
 };
