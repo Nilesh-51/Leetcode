ca0b1a5b370a68acf8a9e8f0d2e4e24df017033f
Nilesh <105011486+Nilesh-51@users.noreply.github.com>
2026-01-23 21:53:06 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.9 MB (41.70%)

--- 0001-two-sum/solution.go
+++ 0001-two-sum/solution.go
@@ -1,15 +1,14 @@
 func twoSum(nums []int, target int) []int {
-    mp := make(map[int]int)
-    var result []int
+    mp:= make(map[int]int)
+    var res []int
     for i, num := range nums {
-        key := target-num
-        if val, ok := mp[key]; ok {
-            result = append(result, val)
-            result = append(result, i)
-            return result
+        if _,ok:=mp[target-num];ok {
+            res=append(res,i)
+            res=append(res,mp[target-num])
+            return res
         } else {
-            mp[num] = i
+            mp[num]=i
         }
     }
-    return result
+    return res
 }
